514e722bc1a553c0468a3393b5fea982d2377d5b
ACER <ACER@LAPTOP-99LCG23L>
2022-12-13 11:41:19 +0700

    Mumet menn lehhh

---
+++ src/main/java/Pertemuan8/User.java
@@ -0,0 +1,22 @@
+package Pertemuan8;
+
+/**
+ *
+ * @author ACER
+ */
+public class User {
+    String email;
+    String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+    
+    public boolean verify(String email, String password) {
+        if(this.email.equals(email) && this.password.equals(password))
+            return true;
+        
+        return false;
+    }
+}
